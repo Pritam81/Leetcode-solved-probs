6e596cfd80f9662d93122af83f9b439e246f1e82
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2023-02-05 22:25:49 +0530

    Time: 5 ms (32.18%), Space: 7.2 MB (40.52%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        reverse(matrix.begin(),matrix.end());
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i;j<matrix[0].size();j++){
+                swap(matrix[i][j],matrix[j][i]);
+
+            }
+        }
+
+
+        
+    }
+};
