86eab07042e2b4517e875bd0155493dab99621e0
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2023-03-01 22:24:39 +0530

    Time: 457 ms (27.42%), Space: 133.8 MB (20.31%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+void outPlaceMerge(vector<int> &nums, int low, int mid, int high) {
+	if (low >= high) return;
+	int l = low, r = mid + 1, k = 0, size = high - low + 1;
+	vector<int> sorted(size, 0);
+	while (l <= mid and r <= high)
+		sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
+	while (l <= mid) 
+		sorted[k++] = nums[l++];
+	while (r <= high) 
+		sorted[k++] = nums[r++];
+	for (k = 0; k < size; k++)
+		nums[k + low] = sorted[k];
+}
+
+void mergeSort(vector<int> &nums, int low, int high) {
+	if (low >= high) return;
+	int mid = (high - low) / 2 + low;
+	mergeSort(nums, low, mid);
+	mergeSort(nums, mid + 1, high);
+	outPlaceMerge(nums, low, mid, high);
+}
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums,0,nums.size()-1);
+        return nums;
+    }
+};
